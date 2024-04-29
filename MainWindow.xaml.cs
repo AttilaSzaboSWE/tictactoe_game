191cb26446063563d2e3ca7c9e319ca8cb356d00
ASzabo5 <attila.szabo3@harman.com>
2024-04-29 15:17:48 +0200

    UI kinézet megvan csinálva

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace tictactoe_game
     /// </summary>
     public partial class MainWindow : Window
     {
+        public enum Player
+        {
+            X, O
+        }
+
+        Player player1;
+        int player1wins_count = 0;
+        int player2wins_count = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            NewGame();
+        }
+
+
+        private void exit(object sender, RoutedEventArgs e) //Kilépés gomb
+        {
+            if (MessageBox.Show("Biztos ki akarsz lépni?", "Megerősítés", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void NewGame() //Új game teljesen lenullázva minden - Új játék gomb
+        {
+            List<Button> buttons = new List<Button> { button1, button2, button3, button4, button5, button6, button7, button8, button9 };
+
+            foreach (Button x in buttons)
+            {
+
+            }
+        }
+
+        private void NextRound() //Nem új game,nem nullázunk le semmit, viszont itt csak a button-öket fogjuk resetelni. - Következő forduló gomb
+        {
+
         }
     }
+       
 }
